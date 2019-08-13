199ddb82a790a32537d7d53dbd405390c985206d
ZYC <zycread@gmail.com>
2019-08-13 20:20:06 +0800

    [ Course 2_20190811 ]

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,5 +1,6 @@
 from django.template.loader import get_template
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from datetime import datetime
 from .models import Post
 
@@ -7,17 +8,20 @@ from .models import Post
 
 
 def homepage(request):
-    # posts = Post.objects.all()
-    # post_lists = list()
-    # for count, post in enumerate(posts):
-    #     post_lists.append("<h3>No.{}:".format(
-    #         str(count)) + str(post)+"</h3>")
-    #     post_lists.append("<small>"+str(post.body)+"</small><br><br>")
-    # return HttpResponse(post_lists)
-
     template = get_template('index.html')
     posts = Post.objects.all()
     now = datetime.now()
     html = template.render(locals())
     return HttpResponse(html)
 
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+        return HttpResponse(html)
+    except:
+        return redirect('/')
+    # pass 不回傳值
